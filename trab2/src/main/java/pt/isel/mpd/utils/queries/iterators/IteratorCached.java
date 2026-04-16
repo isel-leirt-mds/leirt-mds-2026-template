162a8ff67541a2308a2aa7d1700fc164b143ba62
jmartins3 <jmartinsisel.pt>
2026-04-16 13:54:18 +0100

    trab2 initial

---
+++ trab2/src/main/java/pt/isel/mpd/utils/queries/iterators/IteratorCached.java
@@ -0,0 +1,33 @@
+package pt.isel.mpd.utils.queries.iterators;
+
+import java.util.Iterator;
+import java.util.List;
+
+
+public class IteratorCached<T> implements Iterator<T> {
+    private final List<T> cache;
+    private final Iterator<T> itSrc;
+
+    private int index;
+
+
+    public IteratorCached(Iterator<T> itSrc, List<T> cache) {
+        this.itSrc = itSrc;
+        this.cache = cache;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (index < cache.size()) return true;
+        if (!itSrc.hasNext()) return false;
+        cache.add(itSrc.next());
+        return true;
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext())
+            throw new IllegalStateException();
+        return cache.get(index++);
+    }
+}
